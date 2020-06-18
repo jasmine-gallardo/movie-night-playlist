f63153c3a05c9b9912d9ff9b4622a4b076dee5c7
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-06-18 16:39:22 +0000

    Imported User Playlists component.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserPlaylists from './user-playlists';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -18,20 +19,25 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <nav>
-        <div className="nav-wrapper row">
-          <div className="col s11 offset-s1 valign-wrapper">
-            <i className="fas fa-film"></i>
-            <a href="wireframe.html" className="col s6 m6 l8">
-              Movie Night
-            </a>
-            <ul id="nav-mobile" className="right col s5 offset-s1 m4 offset-m2 l3 offset-l1">
-              <li><a href="wireframe.html">Playlists</a></li>
-              <li><a href="user.html"><i className="fas fa-user"></i></a></li>
-            </ul>
+      <div>
+        <nav>
+          <div className="nav-wrapper row">
+            <div className="col s11 offset-s1 valign-wrapper">
+              <i className="fas fa-film"></i>
+              <a href="wireframe.html" className="col s6 m6 l8">
+                Movie Night
+              </a>
+              <ul id="nav-mobile" className="right col s5 offset-s1 m4 offset-m2 l3 offset-l1">
+                <li><a href="wireframe.html">Playlists</a></li>
+                <li><a href="user.html"><i className="fas fa-user"></i></a></li>
+              </ul>
+            </div>
           </div>
+        </nav>
+        <div>
+          <UserPlaylists />
         </div>
-      </nav>
+      </div>
     );
   }
 }
