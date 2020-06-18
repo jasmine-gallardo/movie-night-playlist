1515cab06208bdeac7a6acf4f08cc389364b3571
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-06-18 20:01:37 +0000

    Added Playlist Card component.

---
+++ client/components/playlist-card.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function PlaylistCard(props) {
+  return (
+    <div className="col s12 m4">
+      <div className="card">
+        <div className="card-content center">
+          <h5>{props.playlistName}</h5>
+        </div>
+        <div className="card-action center">
+          <a href="single-playlist.html">Go to Playlist</a>
+          <a href="#" className="dropdown-trigger" data-target="dropdown1">
+            <i className="fas fa-ellipsis-h"></i>
+          </a>
+          <ul id="dropdown1" className="dropdown-content">
+            <li>
+              <a href="#!">Add a movie</a>
+            </li>
+            <li>
+              <a href="#!">Delete playlist</a>
+            </li>
+            <li>
+              <a href="#!">Rename</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
