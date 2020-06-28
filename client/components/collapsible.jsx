e3d418242a0b70c9807295c9862686d00d8bd76a
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-06-28 03:19:33 +0000

    Imported SingleMovie component to dynamically add movie names to collapsible list.

--- client/components/collapsible.jsx
+++ client/components/collapsible.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Collapsible, CollapsibleItem } from 'react-materialize';
 import SingleMovie from './single-movie';
 
-export default (props) => (
+export default props => (
   <Collapsible popout>
     <CollapsibleItem
       expanded={false}
@@ -16,13 +16,11 @@ export default (props) => (
       node="div"
     >
       <ul className="section">
-      {props.moviesArray.map(movie => {
-        return(
-          <SingleMovie key={movie.movieId} movieName={movie.name}/>
-        );
-      })
-
-      }
+        {props.moviesArray.map(movie => {
+          return (
+            <SingleMovie key={movie.movieId} movieName={movie.name}/>
+          );
+        })}
       </ul>
     </CollapsibleItem>
   </Collapsible>
